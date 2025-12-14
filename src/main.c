bdf44b7a9d45bf55ffc572049c2bf0e04a2ab002
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2025-12-14 22:37:31 +0100

    Add main function to demonstrate lib_set and lib_get

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#inlcue <stdio,h>
+#include "lib.h"
+
+void main() {
+  int t=7;
+  lib_set(t);
+  for( int i=1; i<=10 ; i++ ) {
+    printf("%d x %d = %d\n",i,t,lib_get(i) );
+  }
+  return 0;
+}
+
+  
